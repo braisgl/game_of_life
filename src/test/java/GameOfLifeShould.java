6067196315c59e864b1a124c841eec18c4c44c33
brais <brais.garcia@codurance.com>
2022-09-08 12:05:06 +0200

    RED: Cell doesn't die by underpopulation-v2

--- src/test/java/GameOfLifeShould.java
+++ src/test/java/GameOfLifeShould.java
@@ -6,10 +6,10 @@ public class GameOfLifeShould {
     @Test
     public void cell_dies_by_underpopulation(){
         GameOfLife gameOfLife = new GameOfLife();
-        gameOfLife.board.grid[0][0].status=true;
-        gameOfLife.board.grid[0][1].status=false;
-        gameOfLife.board.grid[1][0].status=false;
-        gameOfLife.board.grid[1][1].status=false;
+        gameOfLife.board.grid[0][0]=new Cell(true);
+        gameOfLife.board.grid[0][1]=new Cell(false);
+        gameOfLife.board.grid[1][0]=new Cell(false);
+        gameOfLife.board.grid[1][1]=new Cell(false);
         gameOfLife.nextGen();
         Assertions.assertEquals(false,gameOfLife.board.grid[0][0].status);
     }
